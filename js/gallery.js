d72994044523887b097dd8553600658350235ca4
ar1234567898 <24a_tav@liceum.ztu.edu.ua>
2025-03-17 13:20:48 +0200

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -13,7 +13,7 @@ document.getElementById("right-arrow").addEventListener("click", () => {
   }
   document
     .getElementById("main-image")
-    .setAttribute("src", `img/${arrayOfImages[galleryImage - 1]}.jpeg`);
+    .setAttribute("src", `img/gallery/${arrayOfImages[galleryImage - 1]}.jpeg`);
     console.log(galleryImage);
 });
 
