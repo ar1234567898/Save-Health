913d2c3fe7230c8880385fed2f30bdaef9f60f52
ar1234567898 <24a_tav@liceum.ztu.edu.ua>
2025-04-01 13:01:43 +0300

    Update vitamins.js

--- js/vitamins.js
+++ js/vitamins.js
@@ -1,53 +1,70 @@
+
+
+let arrayOfPhotos = [
+  "vitamin-a",
+  "vitamin-b",
+  "vitamin-c",
+  "vitamin-d",
+  "vitamin-e",
+  "vitamin-omega-3",
+]
+
 let arrayOfVitamins = [
-    {
-      id: "0",
-      title: "Vitamin A",
-      description: "Supports vision, immune function, and skin health.",
-      image: "",
-      rating: 2,
-      type: "Fat-soluble",
-    },
-    {
-      id: "1",
-      title: "Vitamin B",
-      description: "Boosts energy and supports brain function.",
-      image: "",
-      rating: 3,
-      type: "Water-soluble",
-    },
-    {
-      id: "2",
-      title: "Vitamin C",
-      description: "Strengthens the immune system.",
-      image: "",
-      rating: 1,
-      type: "Water-soluble",
-    },
-    {
-      id: "3",
-      title: "Vitamin D",
-      description: "Supports bone health and immune function.",
-      image: "",
-      rating: 4,
-      type: "Fat-soluble",
-    },
-    {
-      id: "4",
-      title: "Vitamin E",
-      description: "Acts as an antioxidant and supports skin health.",
-      image: "",
-      rating: 5,
-      type: "Fat-soluble",
-    },
-    {
-      id: "5",
-      title: "Omega 3",
-      description: "Promotes heart and brain health.",
-      image: "",
-      rating: 5,
-      type: "Essential fatty acid",
-    },
-  ];
+  {
+    rid: 0,
+    id: "001",
+    title: "Vitamin A",
+    description: "Supports vision, immune function, and skin health. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.",
+    image: "",
+    rating: 2,
+    type: "Fat-soluble",
+  },
+  {
+    rid: 1,
+    id: "002",
+    title: "Vitamin B",
+    description: "Boosts energy and supports brain function.",
+    image: "",
+    rating: 3,
+    type: "Water-soluble",
+  },
+  {
+    rid: 2,
+    id: "003",
+    title: "Vitamin C",
+    description: "Strengthens the immune system.",
+    image: "",
+    rating: 1,
+    type: "Water-soluble",
+  },
+  {
+    rid: 3,
+    id: "004",
+    title: "Vitamin D",
+    description: "Supports bone health and immune function.",
+    image: "",
+    rating: 4,
+    type: "Fat-soluble",
+  },
+  {
+    rid: 4,
+    id: "005",
+    title: "Vitamin E",
+    description: "Acts as an antioxidant and supports skin health.",
+    image: "",
+    rating: 5,
+    type: "Fat-soluble",
+  },
+  {
+    rid: 5, 
+    id: "006",
+    title: "Omega 3",
+    description: "Promotes heart and brain health.",
+    image: "",
+    rating: 5,
+    type: "Essential fatty acid",
+  },
+];
 
 console.log(arrayOfVitamins);
 
@@ -55,17 +72,24 @@ arrayOfVitamins.forEach((vitamin) => {
   console.log(vitamin);
 
   let vitaminDiv = document.createElement("div");
-//   vitaminDiv.innerText = vitamin.title;
+  //   vitaminDiv.innerText = vitamin.title;
   vitaminDiv.classList.add("vitamin");
 
   document.getElementById("p-vitamin").appendChild(vitaminDiv);
 
   vitaminDiv.innerHTML = `
+        <p>${vitamin.id}</p>
         <h3>${vitamin.title}</h3>
-        <img src="img/vitamins/${vitamin.id}.png" alt="">
+        <img src='img/vitamins/${arrayOfPhotos[vitamin.rid]}.png' alt="">
         <p>${vitamin.description}</p>
-        <span>${'💖'.repeat(vitamin.rating) + '🤍'.repeat(5 - vitamin.rating)}</span>
-        <p>${vitamin.type}</p>
-        <span>${vitamin.id}</span>
+
+        <div> 
+               <p>${
+                 "💖".repeat(vitamin.rating) + "🤍".repeat(5 - vitamin.rating)
+               }</p>
+        <p>${vitamin.type}</p> 
+        </div>
     `;
 });
+
+
